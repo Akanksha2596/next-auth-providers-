c562f25e2b4bbabf160169451c32876adc49765b
Akanksha2596 <akanksha.yadav@brainvire.com>
2024-03-21 11:33:12 +0530

    layout file

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import "@/styles/globals.css";
+import { ReactNode } from "react";
+import AppBar from "./AppBar";
+import Provider from "./Provider";
+
+interface IProps {
+  children: ReactNode;
+}
+export default function RootLayout({ children }: IProps) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <AppBar />
+          <div className={"min-h-screen "}>{children}</div>
+        </Provider>
+      </body>
+    </html>
+  );
+}
